3c7844076e306c614b7f04530b6f338ffbbfb549
Adrian Roth <adrianroth10@gmail.com>
2017-01-24 09:04:42 +0100

    update on prerequisites

--- scripts/julia/julia.sh
+++ scripts/julia/julia.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if ! command -v ~/.julia/julia >/dev/null; then
 	git clone git://github.com/JuliaLang/julia.git ~/.julia
 
-	package_install libssl1.0.0 libssl-dev
+	package_install libssl1.0.0 libssl-dev gfortran m4
 	cd ~/.julia
 	git checkout release-0.5
 	make
