74a3f2bb7d3e3b95321f3debf36b2efafebe3d02
Adrian Roth <adrianroth10@gmail.com>
2017-02-19 20:38:41 +0100

    added dependency for vim

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../functions.sh
 
-package_install vim git
+package_install vim vim-gtk git
 git_clone_or_pull https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 PULLED=FALSE
 
