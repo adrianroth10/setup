02834cdc53776aa6b548a82f971cff9149819db6
Adrian Roth <adrianroth10@gmail.com>
2017-02-24 23:31:40 +0100

    small fix

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -4,7 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 package_install vim vim-gtk git
 git_clone_or_pull https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-PULLED=FALSE
 
 cp $DIR/../.vimrc ~/.vimrc
 
