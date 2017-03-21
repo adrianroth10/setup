bf1bd3265df3b74299d74948df48c8d85456ce80
Adrian Roth <adrianroth10@gmail.com>
2017-03-21 16:25:15 +0100

    working script

--- scripts/mutt.sh
+++ scripts/mutt.sh
@@ -8,13 +8,52 @@ mkdir -p ~/.mutt/cache/headers
 mkdir -p ~/.mutt/cache/bodies
 touch ~/.mutt/certificates
 
-add_lines ~/.muttrc "set imap_user = \"adrianroth10@gmail.com\"
-set imap_pass =
+printf "Full name: "
+read NAME
 
-set smtp_url = \"smtp://adrianroth10@smtp.gmail.com:587/\"
-set smtp_pass =
-set from = \"adrianroth10@gmail.com\"
-set realname = \"Adrian Roth\"
+printf "Gmail address: "
+read EMAIL
+EMAIL_SUFFIX="${EMAIL%%@*}"
+if [ "${EMAIL##*@}" != "gmail.com" ]; then
+	printf "${RED}This setup only works for gmail accounts\n${NO_COLOR}"
+	exit 1
+fi
+
+printf "Password (put blank if you don't trust the script): "
+read -s PASSWORD
+printf "\n"
+if [ $PASSWORD ]; then
+	rm ~/.password
+	printf "export GMAIL_PASSWORD=$PASSWORD" > ~/.password_temp
+	printf "Set Password for logging into mutt\n"
+	gpg --output ~/.password --symmetric ~/.password_temp
+	rm ~/.password_temp
+
+	printf "#!/bin/sh
+	pwds=\`gpg --decrypt ~/.passwords\` >/dev/null 2>/dev/null
+	eval \"\$pwds\"
+	exec mutt \"\$@\"
+" > ~/.mutt_exec.sh
+	chmod u+rwx ~/.mutt_exec.sh
+else
+	printf "#!/bin/sh
+	exec mutt \"\$@\"
+" > ~/.mutt_exec.sh
+	chmod u+rwx ~/.mutt_exec.sh
+fi
+
+add_lines ~/.bashrc "alias mutt=\"~/.mutt_exec.sh\"
+"
+ 
+rm ~/.muttrc
+add_lines ~/.muttrc "# Basic muttrc for gmail
+set imap_user = \"${EMAIL}\"
+set imap_pass = \"\$GMAIL_PASSWORD\"
+
+set smtp_url = \"smtp://${EMAIL_SUFFIX}@smtp.gmail.com:587/\"
+set smtp_pass = \"\$GMAIL_PASSWORD\"
+set from = \"${EMAIL}\"
+set realname = \"${NAME}\"
 
 set folder = \"imaps://imap.gmail.com:993\"
 set spoolfile = \"+INBOX\"
