b05ecb4bf02bb18161a80d6cb431d66d22a6b547
Adrian Roth <adrianroth10@gmail.com>
2017-05-03 22:22:20 +0200

    fixed vim script

--- scripts/vim/vim.sh
+++ scripts/vim/vim.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../../extras/functions.sh
 
-if [ "$F" == "f" ] || ! command -v vim ; then
+if [ "$F" == "f" ] || [ "$F" == "ff" ] || ! command -v vim ; then
 	$PACKAGE_INSTALL vim vim-gtk git
 	git_clone_or_pull https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
