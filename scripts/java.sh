cf3f2aec8392827079dad216f1c0eb4a13840ccd
Adrian Roth <adrianroth10@gmail.com>
2017-07-17 17:26:37 +0200

    java 8 introduced

--- scripts/java.sh
+++ scripts/java.sh
@@ -2,4 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
-$PACKAGE_INSTALL default-jdk default-jre
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
