f881ed5cda8b29d4a0509807e9723c8d458820cf
Adrian Roth <adrianroth10@gmail.com>
2017-10-22 16:09:52 +0200

    cuda seems to be working

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 # Installation of the cuda drivers
-# Will probably only work on an ubuntu system
+# Will probably only work on an ubuntu system with certain GPU:s
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
 $PACKAGE_INSTALL linux-headers-$(uname -r)
-sudo dpkg --install cuda-repo-<distro>-<version>.<architecture>.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/<distro>/<architecture>/7fa2af80.pub
-$PACKAGE_UPDATE
-$PACKAGE_INSTALL cuda
-
-add_lines ~/.bashrc "export PATH=/usr/local/cuda-9.0/bin\${PATH:+:\${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}"
-
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get update
-sudo apt-get install cuda
+$PACKAGE_UPDATE
+$PACKAGE_INSTALL cuda
+rm cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+
+add_lines ~/.bashrc "export PATH=/usr/local/cuda-9.0/bin\${PATH:+:\${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}"
+
