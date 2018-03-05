55f40842e46ceb393a15dfd8ea9d160d099ef573
Adrian Roth <adrianroth10@gmail.com>
2018-03-05 11:40:24 +0100

    updated output and return of music

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ LOG=$DIR/setup.log
 function error_trap {
 	printf "\n${RED}	Error occured!"
 	printf "\n	See setup.log\n${NO_COLOR}"
-	#pkill vlc
+	pkill vlc
 	if [ $PID ]; then
 		kill $PID
 	fi
@@ -36,7 +36,7 @@ if ! command -v vlc >/dev/null; then
 fi
 if ! ps -e | grep vlc > /dev/null; then
 	printf "${WHITE}Turning on some music\n${NO_COLOR}"
-	#cvlc --quiet -L $DIR/background_music/* >>$LOG 2>>$LOG & 
+	cvlc --quiet -L $DIR/background_music/* >>$LOG 2>>$LOG & 
 fi
 
 printf "${WHITE}apt-get update and upgrade\n${NO_COLOR}"
@@ -78,7 +78,7 @@ do
 	${SCRIPTS[$i]} -$F >>$LOG 2>>$LOG
 
 	kill $PID
-	printf "${CLEAR_LINE}${GREEN}	%-30s %-30s\n${NO_COLOR}" "$FILE done" $(date -u --date @$((`date +%s` - $DATE)) +%H:%M:%S)
+	printf "${CURSOR_UP}${CLEAR_LINE}${GREEN}	%-30s %-30s\n\n${NO_COLOR}" "$FILE done" $(date -u --date @$((`date +%s` - $DATE)) +%H:%M:%S)
 done
 
 L=${#OTHER[@]}
@@ -91,10 +91,10 @@ if (("$L" > "0")); then
 	$PACKAGE_INSTALL ${OTHER[@]} >>$LOG 2>>$LOG
 
 	kill $PID
-	printf "${CLEAR_LINE}${GREEN}	%-30s %-30s\n${NO_COLOR}" "Other packages done" $(date -u --date @$((`date +%s` - $DATE)) +%H:%M:%S)
+	printf "${CURSOR_UP}${CLEAR_LINE}${GREEN}	%-30s %-30s\n\n${NO_COLOR}" "Other packages done" $(date -u --date @$((`date +%s` - $DATE)) +%H:%M:%S)
 fi
 
-printf "${CLEAR_LINE}${GREEN}\n	%-30s %-30s\n${NO_COLOR}" "All done" $(date -u --date @$((`date +%s` - $START)) +%H:%M:%S)
+printf "${CURSOR_UP}${CLEAR_LINE}${GREEN}\n	%-30s %-30s\n\n${NO_COLOR}" "All done" $(date -u --date @$((`date +%s` - $START)) +%H:%M:%S)
 
 # Kill the music
 pkill vlc
