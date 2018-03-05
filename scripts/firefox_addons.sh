dd3e9e8557eccc0a77ad48deeb1526b677ba1cc1
Adrian Roth <adrianroth10@gmail.com>
2018-03-05 10:40:57 +0100

    failed update of firefox_addons

--- scripts/firefox_addons.sh
+++ scripts/firefox_addons.sh
@@ -2,7 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
-#Following: https://askubuntu.com/questions/492258/installing-a-firefox-extension-via-terminal
-wget https://addons.mozilla.org/firefox/downloads/latest/vimfx/addon-404785-latest.xpi?src=dp-btn-primary
-
-
+# Following: https://askubuntu.com/questions/492258/installing-a-firefox-extension-via-terminal
+# Not working!
+EXTENSION=vimium-1.36-fx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/141716/$EXTENSION
+firefox-esr $EXTENSION
+rm $EXTENSION
