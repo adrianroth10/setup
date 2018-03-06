4ca5e6900dbb6b189e15a1b47dce19883a4f6538
Adrian Roth <adrianroth10@gmail.com>
2018-03-06 20:57:47 +0100

    apache and mysql working

---
+++ scripts/apache.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Installing the apache server with php compatibility
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/../extras/functions.sh
+
+sudo apt-add-repository -y ppa:ondrej/php
+$PACKAGE_UPDATE
+$PACKAGE_INSTALL apache2 php
+
+sudo /bin/sh -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
+sudo service apache2 restart
+cd /var/www/
+sudo chown -R $(id -un):www-data html
--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -3,5 +3,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
 # Will not work in the setup script!!
-$PACKAGE_INSTALL mysql-server
-sudo mysql_install_db
+$PACKAGE_INSTALL mysql-server php-mysql
+
+# Might not work
+while true; do
+    read -p "Do you wish to add basic user?" yn
+    case $yn in
+        [Yy]* ) echo "MySql Root user"
+		mysql -u root -p -e "CREATE USER '$(id -un)'@'localhost' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO '$(id -un)'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;";;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
