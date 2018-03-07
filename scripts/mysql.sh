dc1dc294d4fda313abaec6f5aa494b67f4c0db97
Adrian Roth <adrianroth10@gmail.com>
2018-03-07 16:56:53 +0100

    removed adding user in mysql

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -4,14 +4,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Will not work in the setup script!!
 $PACKAGE_INSTALL mysql-server php-mysql
-
-# Might not work
-while true; do
-    read -p "Do you wish to add basic user?" yn
-    case $yn in
-        [Yy]* ) echo "MySql Root user"
-		mysql -u root -p -e "CREATE USER '$(id -un)'@'localhost' IDENTIFIED BY ''; GRANT ALL PRIVILEGES ON *.* TO '$(id -un)'@'localhost' WITH GRANT OPTION; FLUSH PRIVILEGES;";;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
