afb456fd07501c0cc981ab5ed8b350eb4cee005f
Adrian Roth <adrianroth10@gmail.com>
2018-05-23 14:45:00 +0200

    ubuntu 18 compability

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
-snap install spotify
+sudo snap install spotify
 # if ! command -v spotify ; then
 # 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 # 	echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
