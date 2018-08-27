014ff825b72dbe92e367f3d132f526a0da5d57df
Anton Såmark-Roth <anton.samark-roth@nuclear.lu.se>
2018-08-27 14:15:07 +0200

    Further updates to for pyne.

--- scripts/for_pyne.sh
+++ scripts/for_pyne.sh
@@ -3,7 +3,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/../extras/functions.sh
 
 conda install -y -c anaconda pip
+conda-build jinja2 nose setuptools pytables hdf5 scipy patchelf
 
 sudo $(which pip) --no-cache-dir install \
+	numpydoc \ 
+	prettytable \
 	pyrax \
-	xdress
+	xdress \
+	cloud_sptheme \
+	sphinxcontrib-bibtex
