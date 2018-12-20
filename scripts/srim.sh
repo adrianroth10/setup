f48df0e291133a3b74b6e36ffe349856f7b2ac78
Anton Såmark-Roth <anton.samarkroth@gmail.com>
2018-12-20 07:25:36 +0100

    SRIM script remod. Download of pysrim git repo included.

--- scripts/srim.sh
+++ scripts/srim.sh
@@ -13,4 +13,41 @@ SRIMDIR=~/.srim
 
 mkdir -p $SRIMDIR
 curl --output $SRIMDIR/SRIM_INSTALL.exe http://www.srim.org/SRIM/SRIM-2013-Std.e
+#curl --output $SRIMDIR/SRIM_INSTALL.exe http://www.srim.org/SRIM/SRIM-2008-Std.e
 wine $SRIMDIR/SRIM_INSTALL.exe
+
+# Recipe
+# Try the 2008 version, first. Extract the files into the c directory in .wine in program_files and in a directory SRIM-2008. From this directory now execute the setup.exe file and install SRIM to the same path. 
+# THIS actually worked, once!
+# Following this I followed the lines above to install SRIM-2013 in the ~/.srim directory, and it also worked.
+
+VERSION="geant4-10.5-release"
+
+if [ "$F" == "ff" ]; then
+	rm -rf ~/.pysrim 
+fi
+
+if [ "$F" == "f" ] || [ "$F" == "ff" ]; then
+	if [ -d ~/.pysrim ]; then
+		cd ~/.pysrim
+		git gc --prune=now
+		git remote prune origin
+		#git checkout $VERSION
+		git fetch --all
+		#git reset --hard origin/$VERSION #this was fatal for some reason
+		#git pull https://github.com/Geant4/geant4.git ~/.geant4 $VERSION
+		#sudo rm -rf $VERSION-build
+		#sudo rm -rf $VERSION-install
+	else
+		#Download source files
+		#wget -O ~/.geant4/ "http://cern.ch/geant4/support/source/geant4.10.03.p01.tar.gz"
+		#or clone: 
+		git_clone_or_pull https://gitlab.com/costrouc/pysrim.git ~/.pysrim
+		cd ~/.pysrim
+		#git checkout $VERSION
+	fi
+
+	#sudo docker run -v $PWD/examples/docker:/opt/pysrim/ \
+	#           -v /tmp/output:/tmp/output \
+	#	              -it costrouc/pysrim sh -c "xvfb-run -a python3.6 /opt/pysrim/ni.py"
+fi
