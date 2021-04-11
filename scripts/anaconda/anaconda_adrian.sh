81022c0cec26515dfbe80c77f6cb4149708d1024
Adrian Roth <roth.adrian@protonmail.com>
2021-04-11 21:08:17 +0200

    new installation

---
+++ scripts/anaconda/anaconda_adrian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/../extras/functions.sh
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod u+x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+source /home/adrian/.bashrc
+
+conda env update -f environment_adrian.yml
--- scripts/neovim/neovim.sh
+++ scripts/neovim/neovim.sh
@@ -4,9 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source=/dev/null
 . "$DIR/../../extras/functions.sh"
 
-$PACKAGE_INSTALL neovim xsel
-sudo -H pip3 install pynvim
+# install minicode before this script if you want it
 # This action require prior installation of npm
+$PACKAGE_INSTALL neovim xsel
+pip install pynvim
 sudo npm install --global neovim
 
 mkdir -p ~/.config/nvim/spell
@@ -14,18 +15,17 @@ cp "$DIR/init.vim" ~/.config/nvim/
 
 #### Plugins ####
 # Vim-Plug as package manager
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs' \
-      'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim +PlugInstall +qall >/dev/null
 
 # Installing Linters for ale
 $PACKAGE_INSTALL shellcheck
 $PACKAGE_INSTALL lacheck
-pip3 install --user pylint
+pip install pylint
 
 ### Building YouCompleteMe ####
-$PACKAGE_INSTALL build-essential cmake python3-dev dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+$PACKAGE_INSTALL build-essential cmake python3-dev dirmngr gnupg apt-transport-https ca-certificates software-properties-common default-jre
 
 # install mono-complete
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
@@ -44,3 +44,4 @@ export PATH=$PATH:/usr/local/go/bin
 # compile the plugin
 cd ~/.vim/plugged/youcompleteme || exit
 python3 install.py --all
+###
